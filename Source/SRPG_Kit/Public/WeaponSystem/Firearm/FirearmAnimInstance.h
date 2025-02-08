39e61b237176dbe91bea82e4abd79175f1a6909b
Timalice1 <newacccc2@gmail.com>
2025-02-08 11:01:14 +0200

    Update weapon anim instance class

--- Source/SRPG_Kit/Public/WeaponSystem/Firearm/FirearmAnimInstance.h
+++ Source/SRPG_Kit/Public/WeaponSystem/Firearm/FirearmAnimInstance.h
@@ -10,14 +10,21 @@ class SRPG_KIT_API UFirearmAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 
 public:
+	virtual void NativeBeginPlay() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	virtual void Shoot();
+
+private:
+	UPROPERTY()
+	TObjectPtr<class AActor> _ownerActor = nullptr;
+
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "FireWeapon|Slider")
 	FVector TargetSlideTranslation = FVector();
+
 	UPROPERTY(EditAnywhere, Category = "FireWeapon|Slider")
 	float SlideTranslationInterpSpeed = 10.f;
 
-private:
+	UPROPERTY(BlueprintReadOnly, Category = "FireWeapon|Slider")
 	float SlideTranslationAlpha = 0.f;
 };
