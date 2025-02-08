0a0d3398422b415ecef15cd7ed494f8f6d236647
Timalice1 <newacccc2@gmail.com>
2025-02-08 09:41:54 +0200

    Add fire weapon anim instance class

---
+++ Source/SRPG_Kit/Public/WeaponSystem/Firearm/FirearmAnimInstance.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "FirearmAnimInstance.generated.h"
+
+UCLASS()
+class SRPG_KIT_API UFirearmAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void Shoot();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "FireWeapon|Slider")
+	FVector TargetSlideTranslation = FVector();
+	UPROPERTY(EditAnywhere, Category = "FireWeapon|Slider")
+	float SlideTranslationAlpha = 0.f;
+};
