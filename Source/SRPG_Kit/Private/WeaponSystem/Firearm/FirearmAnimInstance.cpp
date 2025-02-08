15a64fb421c29218b11d5428e21b211d9bd44e9d
Timalice1 <newacccc2@gmail.com>
2025-02-08 11:01:14 +0200

    Update weapon anim instance class

--- Source/SRPG_Kit/Private/WeaponSystem/Firearm/FirearmAnimInstance.cpp
+++ Source/SRPG_Kit/Private/WeaponSystem/Firearm/FirearmAnimInstance.cpp
@@ -1,8 +1,16 @@
 #include "WeaponSystem/Firearm/FirearmAnimInstance.h"
+#include "WeaponSystem/Firearm/FireWeaponInterface.h"
+
+void UFirearmAnimInstance::NativeBeginPlay()
+{
+    if (GetOwningActor()->GetClass()->ImplementsInterface(UFireWeaponInterface::StaticClass()))
+        _ownerActor = GetOwningActor();
+}
 
 void UFirearmAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
-    SlideTranslationAlpha = FMath::FInterpTo(SlideTranslationAlpha, 0.f, DeltaSeconds, SlideTranslationInterpSpeed);
+    if (_ownerActor && IFireWeaponInterface::Execute_GetCurrentAmmo(_ownerActor) != 0)
+        SlideTranslationAlpha = FMath::FInterpTo(SlideTranslationAlpha, 0.f, DeltaSeconds, SlideTranslationInterpSpeed);
 }
 
 void UFirearmAnimInstance::Shoot()
